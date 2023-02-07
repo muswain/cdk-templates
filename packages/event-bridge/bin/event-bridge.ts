69889c74888ca4ec18d19e16ebb73caaeeb7b9c0
muswain <mrutyunjay.swain@gmail.com>
2023-02-07 22:27:00 +0800

    feat: :sparkles: add tag for all CDK resources

--- packages/event-bridge/bin/event-bridge.ts
+++ packages/event-bridge/bin/event-bridge.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
+import { Tags } from 'aws-cdk-lib';
 import { EventBridgeStack } from '../lib/event-bridge-stack';
 
 const app = new cdk.App();
+Tags.of(app).add('name', 'mj'); // Adds tag for the complete application
 new EventBridgeStack(app, 'EventBridgeStack');
